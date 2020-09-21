926ab8788487c42e8e4f3a41f9da303fd0c1c490
Vlp90 <vladimir.leloup@gmail.com>
2020-09-21 10:25:09 +0200

    header first part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-     
+      <div className="app__header">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="instagram_logo"
+          className="app__headerImage"
+        />
+      </div>
     </div>
   );
 }
