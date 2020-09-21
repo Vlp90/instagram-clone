e94af40b92596e689a5e4b3de747ee2e55dade86
Vlp90 <vladimir.leloup@gmail.com>
2020-09-21 11:30:39 +0200

    connecting firebae to backend

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBlUIbXuQV_Up3Psbwnxfp-fux7Z7MzzLg",
+    authDomain: "instagram-light-1b746.firebaseapp.com",
+    databaseURL: "https://instagram-light-1b746.firebaseio.com",
+    projectId: "instagram-light-1b746",
+    storageBucket: "instagram-light-1b746.appspot.com",
+    messagingSenderId: "363693175893",
+    appId: "1:363693175893:web:a87988923f68086b3e10dc",
+    measurementId: "G-5MJFS8WDFW"
+  };
